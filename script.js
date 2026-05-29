6bd3fb145118ad09706e6ac278f7cace63979209
Ricardo Augusto de Matos Filho <ricardo.filho@fiap.com.br>
2026-05-29 09:48:06 -0300

    improved live dashboard

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 function randomTemp() {
-  return Math.floor(Math.random() * 12) + 24;
+  return Math.floor(Math.random() * 14) + 22;
 }
 
 function randomHumidity() {
-  return Math.floor(Math.random() * 30) + 40;
+  return Math.floor(Math.random() * 40) + 35;
+}
+
+function randomSatellite() {
+  const status = [
+    "Operacional",
+    "Escaneando",
+    "Transmitindo",
+    "Online"
+  ];
+
+  return status[Math.floor(Math.random() * status.length)];
 }
 
 function updateDashboard() {
@@ -16,14 +27,22 @@ function updateDashboard() {
 
   const fire = document.getElementById("fire");
 
-  if(temp >= 32){
-    fire.innerText = "Alto";
-  } else if(temp >= 28){
+  if(temp >= 33){
+    fire.innerText = "Crítico";
+  }
+  else if(temp >= 29){
     fire.innerText = "Moderado";
-  } else {
+  }
+  else{
     fire.innerText = "Baixo";
   }
 
+  const statusText = document.querySelector(".online");
+
+  statusText.innerText = randomSatellite();
+
 }
 
-setInterval(updateDashboard, 4000);
+updateDashboard();
+
+setInterval(updateDashboard, 3000);
